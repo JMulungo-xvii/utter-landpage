fd25e6aa2b0ffb8df906c1743c0724cf1b5d39ff
JMulungo-xvii <jacintoenigmamulungo@gmail.com>
2021-06-10 05:14:51 +0200

    add visao, proposito, contexto

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,3 +312,60 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+// popup 2
+// Get the modal
+var modal_2 = document.getElementById("myModal_2");
+
+// Get the button that opens the modal
+var btn_2 = document.getElementById("myBtn_2");
+
+// Get the <span> element that closes the modal
+var span_2 = document.getElementsByClassName("close_2")[0];
+
+// When the user clicks the button, open the modal 
+btn_2.onclick = function() {
+  modal_2.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span_2.onclick = function() {
+  modal_2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal_2) {
+    modal_2.style.display = "none";
+  }
+}
+
+
+// popup 2
+// Get the modal
+var modal_3 = document.getElementById("myModal_3");
+
+// Get the button that opens the modal
+var btn_3 = document.getElementById("myBtn_3");
+
+// Get the <span> element that closes the modal
+var span_3 = document.getElementsByClassName("close_3")[0];
+
+// When the user clicks the button, open the modal 
+btn_3.onclick = function() {
+  modal_3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span_3.onclick = function() {
+  modal_3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal_3) {
+    modal_3.style.display = "none";
+  }
+}
